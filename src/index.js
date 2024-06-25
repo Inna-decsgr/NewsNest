75c418c89e5eec70cb1efa2dd88e4d8cc35344f0
Inna-decsgr <anjgkwl27@gmail.com>
2024-06-26 01:39:42 +0900

    Set up basic app structure and configure routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import News from './pages/News';
+import NewsArticle from './pages/NewsArticle';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true, element: <News />
+      },
+      {
+        path: '/news', element: <News />
+      },
+      {
+        path: '/news/:keyword',
+        element: <News />
+      },
+      {
+        path: '/news/article/:category',
+        element: <NewsArticle />
+      }
+    ]
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
