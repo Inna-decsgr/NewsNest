e12ed3a23d245053b95d3720751cdf9806cae9a9
Inna-decsgr <anjgkwl27@gmail.com>
2024-06-28 00:44:53 +0900

    Delete Sidebar.jsx and clean up unused files and resources

--- src/components/NewsHome.jsx
+++ src/components/NewsHome.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import NewsCard from './NewsCard';
-import SideBar from './SideBar';
-import { useParams } from 'react-router-dom';
 
 export default function NewsHome({ newsdata }) {
-  const { keyword } = useParams();
-
   return (
     <section className='flex flex-col lg:flex-row'>
       <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 gap-y-4'>
@@ -15,11 +11,6 @@ export default function NewsHome({ newsdata }) {
           ))
         }
       </ul>
-      {!keyword && (
-        <div className='ml-4'>
-          <SideBar />
-        </div>
-      )}
     </section>
   );
 }
